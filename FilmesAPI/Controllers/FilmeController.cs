d703e3951e2574e37bd991a04a1a1f1ab44e49a6
Solemon <solemon.00@gmail.com>
2023-03-24 23:27:42 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new();
+
+
+
+        //[FromBody] -> especifica que parametro do Post vem através do corpo da requisição
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required (ErrorMessage ="O filme deve possuir um nome")]
+        [MaxLength(60,ErrorMessage ="O nome do filme está muito grande")]
+        [MinLength(3, ErrorMessage = "O nome do filme não deve ser tão pequeno")]
+        public string Titulo { get; set; }
+
+
+        [Required(ErrorMessage = "O filme deve possuir um gênero")]
+        [MaxLength(60, ErrorMessage = "O gênero do filme está muito grande")]
+        [MinLength(3, ErrorMessage = "O gênero do filme não deve ser tão pequeno")]
+        public string Genero { get; set; }
+
+
+        [Required(ErrorMessage = "O filme deve possuir uma duração")]
+        [Range(50, 600, ErrorMessage = "A duração do filme deve estar entre 50 e 600 minutoa")]
+        public int Duracao { get; set; }
+    }
+}
